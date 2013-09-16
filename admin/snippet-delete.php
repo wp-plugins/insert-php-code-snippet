e66d0e132bae066425d1fe6806dfa887ced9df0c
f1logic <f1logic@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-16 09:12:31 +0000

    insert-php-code-snippet version 1.0 release
    git-svn-id: https://plugins.svn.wordpress.org/insert-php-code-snippet/tags/1.0@773191 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin/snippet-delete.php
@@ -0,0 +1,28 @@
+<?php
+global $wpdb;
+
+$_POST = stripslashes_deep($_POST);
+$_GET = stripslashes_deep($_GET);
+
+$xyz_ips_snippetId = intval($_GET['snippetId']);
+$xyz_ips_pageno = intval($_GET['pageno']);
+
+if($xyz_ips_snippetId=="" || !is_numeric($xyz_ips_snippetId)){
+	header("Location:".admin_url('admin.php?page=insert-php-code-snippet-manage'));
+	exit();
+
+}
+$snippetCount = $wpdb->query( 'SELECT * FROM '.$wpdb->prefix.'xyz_ips_short_code WHERE id="'.$xyz_ips_snippetId.'" LIMIT 0,1' ) ;
+
+if($snippetCount==0){
+	header("Location:".admin_url('admin.php?page=insert-php-code-snippet-manage&xyz_ips_msg=2'));
+	exit();
+}else{
+	
+	$wpdb->query( 'DELETE FROM  '.$wpdb->prefix.'xyz_ips_short_code  WHERE id="'.$xyz_ips_snippetId.'" ' ) ;
+	
+	header("Location:".admin_url('admin.php?page=insert-php-code-snippet-manage&xyz_ips_msg=3&pagenum='.$xyz_ips_pageno));
+	exit();
+	
+}
+?>
